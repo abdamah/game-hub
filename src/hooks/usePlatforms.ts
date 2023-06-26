3a1b7b25a60306c21f6ba9dd4522fa133ba03211
Abdallah Mah <abdamoh1@gmail.com>
2023-06-27 02:23:19 +0530

    Ship platforms with the app

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,5 +1,4 @@
-import useData from './useData';
-import { Platform } from './useGames';
-const usePlatforms = () => useData<Platform>('/platforms/lists/parents');
+import platforms from '../data/platforms';
+const usePlatforms = () => ({ data: platforms, isLoading: false, error: null });
 
 export default usePlatforms;
