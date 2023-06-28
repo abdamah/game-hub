771379fbe3775847675f8887faeab8812c12f207
Abdallah Mah <abdamoh1@gmail.com>
2023-06-29 03:43:47 +0530

    Refactor: move entity interfaces

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -3,20 +3,9 @@ import APIClient, { FetchResponse } from '../services/api-client';
 
 import ms from 'ms';
 import useGameQueryStore from '../store';
-import { Platform } from './usePlatforms';
+import { Game } from '../entities/Game';
 
 const apiClient = new APIClient<Game>('/games');
-
-export interface Game {
-  id: number;
-  name: string;
-  slug: string;
-  description_raw: string;
-  background_image: string;
-  parent_platforms: { platform: Platform }[];
-  metacritic: number;
-  rating_top: number;
-}
 
 const useGames = () => {
   const gameQuery = useGameQueryStore((s) => s.gameQuery);
