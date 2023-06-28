5c20566c614ceb30c0e8a471bda4ecb8fcf75d7f
Abdallah Mah <abdamoh1@gmail.com>
2023-06-29 04:19:24 +0530

    Build expandable text

---
+++ src/components/ExpandableText.tsx
@@ -0,0 +1,35 @@
+import { Button, Text } from '@chakra-ui/react';
+import { useState } from 'react';
+
+interface Props {
+  children: string;
+}
+
+const ExpandableText = ({ children }: Props) => {
+  const [expanded, setExpanded] = useState(false);
+
+  if (!children) return null;
+
+  const limit = 300;
+
+  if (children.length <= limit) return <Text>{children}</Text>;
+
+  const summary = expanded ? children : children.substring(0, limit) + '...';
+
+  return (
+    <Text>
+      {summary}
+      <Button
+        onClick={() => setExpanded(!expanded)}
+        size='xs'
+        fontWeight='bold'
+        colorScheme='yellow'
+        marginLeft={1}
+      >
+        {expanded ? 'Show Less' : 'Read More'}
+      </Button>
+    </Text>
+  );
+};
+
+export default ExpandableText;
